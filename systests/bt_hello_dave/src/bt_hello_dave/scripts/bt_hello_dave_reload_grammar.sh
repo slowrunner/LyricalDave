43c6f1a9b1917d919fdffb4e808699f2619fb763
slowrunner <slowrunner@users.noreply.github.com>
2026-06-14 06:42:21 -0400

    Delete systests/bt_hello_dave/src/bt_hello_dave directory

--- systests/bt_hello_dave/src/bt_hello_dave/scripts/bt_hello_dave_reload_grammar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# bt_hello_dave_reload_grammar.sh
-#
-# Hot-reloads the grammar YAML (fixed responses, random pool, grammar
-# list) without restarting the BT system. Also re-pushes the grammar
-# list to /stt/set_grammar.
-
-ros2 service call /hello_dave_bt/reload_grammar std_srvs/srv/Trigger {}
