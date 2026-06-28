100d7c63cc612b9a7c06ef38a6ca44af6e758eb4
slowrunner <slowrunner@users.noreply.github.com>
2026-06-28 12:38:06 -0400

    tts addition to manifest

---
+++ systests/urml/mygopigo3/validate_test_manifest.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "urml validate --manifest test.manifest.yaml test_emit_speak.program.urml.yaml --profile educational"
+urml validate --manifest test.manifest.yaml test_emit_speak.program.urml.yaml --profile educational
